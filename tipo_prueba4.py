15a7b458a472d0eb34b35b986fe4ad612ade1731
Renato Colipe Lienan <re.colipe@duocuc.cl>
2025-06-23 17:05:28 -0400

    quitamos los comentarios en python para dejarlos como commit en github

--- tipo_prueba4.py
+++ tipo_prueba4.py
@@ -2,14 +2,14 @@
 estudiantes ={} 
 
 
-# registro de estudiantes por nombre y  validacion de nombre
+
 
 def resgistrar_estudiante():
     nombre = input("Ingrese el nombre del estudiante:")
     
     if any(i['nombre'].lower()==nombre.lower() for i in estudiantes.values()):
             print( "ERROR: El estudiante ya existe")
-    # se pide al usuario su edad yse valida que este en el rango         
+     
     try:
         edad = int(input("Ingrese la edad del estudiante:"))
         if edad<12 or edad>80:
