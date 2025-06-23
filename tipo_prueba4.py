b5acdf1a8b77ab12438b8a1b51d203ab146b1a79
Renato Colipe Lienan <re.colipe@duocuc.cl>
2025-06-23 16:31:34 -0400

    se piden los datos y se valida antes de guardarlos

--- tipo_prueba4.py
+++ tipo_prueba4.py
@@ -2,3 +2,12 @@
 estudiantes ={} 
 
 
+# registro de estudiantes por nombre y  validacion de nombre
+
+def resgistrar_estudiante():
+    nombre = input("Ingrese el nombre del estudiante:")
+    
+    if any(i['nombre'].lower()==nombre.lower() for i in estudiantes.values()):
+            print( "ERROR: El estudiante ya existe")
+  
+
